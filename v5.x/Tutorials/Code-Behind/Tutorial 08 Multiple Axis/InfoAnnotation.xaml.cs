b3ef40e85afd5552d0372e00b981fb4cfce5e174
andyb1979 <aburnettthompson@gmail.com>
2017-11-06 15:27:12 +0000

    Added v5 Code Behind tutorials to

---
+++ v5.x/Tutorials/Code-Behind/Tutorial 08 Multiple Axis/InfoAnnotation.xaml.cs
@@ -0,0 +1,11 @@
+﻿using SciChart.Charting.Visuals.Annotations;
+namespace SciChart.Tutorial
+{
+    public partial class InfoAnnotation : CustomAnnotation
+    {
+        public InfoAnnotation()
+        {
+            InitializeComponent();
+        }
+    }
+}
