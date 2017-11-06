741473f7e3abfc7f3d0b8e4cb2895ec0fc6e1084
andyb1979 <aburnettthompson@gmail.com>
2017-11-06 16:15:19 +0000

    Added Tutorial 4 MVVM / Zooming panning

---
+++ v5.x/Tutorials/MVVM/Tutorial 04b Adding Zooming Panning with MVVM/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SciChart.Mvvm.Tutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
