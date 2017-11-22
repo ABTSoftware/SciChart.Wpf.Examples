62722ea158f0991eaf1c51fd56c8369cb7668bd8
andyb1979 <aburnettthompson@gmail.com>
2017-11-22 15:13:14 +0000

    Merging sandbox from v4 to v5

---
+++ v5.x/Sandbox/Examples/CustomModifiersSandbox/CustomModifierSandboxViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SciChart.Charting.Model.DataSeries;
+using SciChart.Charting.Visuals.RenderableSeries;
+using SciChart.Data.Model;
+using SciChart.Sandbox.Shared;
+
+namespace SciChart.Sandbox.ExampleSandbox.CustomModifiers
+{
+    public class CustomModifierSandboxViewModel : BindableObject
+    {
+        private IDictionary<IRenderableSeries, List<DataPoint>> _selectedDataPoints;
+        private XyDataSeries<DateTime, double> _bidDataSeries = new XyDataSeries<DateTime, double>() { SeriesName = "Bid"};
+        private XyDataSeries<DateTime, double> _offerDataSeries = new XyDataSeries<DateTime, double>() { SeriesName = "Offer" };
+
+        public CustomModifierSandboxViewModel()
+        {
+            // Generate some data
+            var r = new RandomWalkGenerator();
+            var data = r.GetRandomWalkSeries(500);
+            _bidDataSeries.Append(data.XData.Select(x => DateTime.Today.AddDays(x)), data.YData);
+
+            r = new RandomWalkGenerator();
+            data = r.GetRandomWalkSeries(500);
+            _offerDataSeries.Append(data.XData.Select(x => DateTime.Today.AddDays(x)), data.YData);
+        }
+
+        public XyDataSeries<DateTime, double> BidDataSeries { get { return _bidDataSeries; } }
+
+        public XyDataSeries<DateTime, double> OfferDataSeries { get { return _offerDataSeries; } } 
+
+
+        /// <summary>
+        /// Gets or sets the SelectedDataPoints. This is bound to SimpleDataPointSelectionModifier.SelectedPoints
+        /// </summary>
+        public IDictionary<IRenderableSeries, List<DataPoint>> SelectedDataPoints
+        {
+            get { return _selectedDataPoints; }
+            set
+            {
+                if (value != _selectedDataPoints)
+                {
+                    _selectedDataPoints = value;
+                    OnPropertyChanged("SelectedDataPoints");
+
+                    // TODO HERE: React to Selection Changed if you want 
+                }
+            }
+        }
+    }
+}
---
+++ v5.x/Sandbox/Examples/CustomModifiersSandbox/SimpleSeriesDragModifier.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using SciChart.Charting.ChartModifiers;
+using SciChart.Charting.Model.DataSeries;
+using SciChart.Charting.Visuals.RenderableSeries;
+using SciChart.Core.Utility.Mouse;
+
+namespace SciChart.Sandbox.ExampleSandbox.CustomModifiers
+{
+    /// <summary>
+    /// Custom modifier which demonstrates how we can select & drag a series in the Y direction
+    /// </summary>
+    public class SimpleSeriesDragModifier : SeriesSelectionModifier
+    {
+        private IRenderableSeries _selectedSeries;
+        private Point _lastMousePoint;
+
+        public override void OnModifierMouseDown(ModifierMouseArgs e)
+        {            
+            // Deliberately call OnModifierMouseUp in MouseDown handler to perform selection
+            base.OnModifierMouseUp(e);
+
+            var selectedSeries = this.ParentSurface.RenderableSeries.FirstOrDefault(x => x.IsSelected);
+
+            if (selectedSeries == null)
+            {
+                e.Handled = false;
+                return;
+            }
+
+            _selectedSeries = selectedSeries;
+            _lastMousePoint = e.MousePoint;
+        }
+
+        public override void OnModifierMouseMove(ModifierMouseArgs e)
+        {
+            base.OnModifierMouseMove(e);
+
+            if (_selectedSeries == null)
+            {
+                e.Handled = false;
+                return;
+            };
+
+            var dataSeries = _selectedSeries.DataSeries as XyDataSeries<DateTime, double>;
+            if (dataSeries == null)
+            {
+                throw new InvalidOperationException("This modifier is only configured to work with XyDataSeries<double,double>");
+            }
+
+            var currentMousePoint = e.MousePoint;
+            double currentYValue = _selectedSeries.YAxis.GetCurrentCoordinateCalculator().GetDataValue(currentMousePoint.Y);
+            double startYValue = _selectedSeries.YAxis.GetCurrentCoordinateCalculator().GetDataValue(_lastMousePoint.Y);
+
+            var deltaY = currentYValue - startYValue;
+
+            OffsetSeries(dataSeries, deltaY);
+
+            _lastMousePoint = currentMousePoint;
+        }
+
+        public void OffsetSeries(XyDataSeries<DateTime, double> series, double offset)
+        {
+            var originalXData = series.XValues.ToArray();
+            var originalYData = series.YValues.ToArray();
+
+            // To Offset the series, clear and recreate data
+            using (series.SuspendUpdates())
+            {
+                series.Clear();
+                series.Append(originalXData, originalYData.Select(y => y + offset));
+            }
+        }
+
+        public override void OnModifierMouseUp(ModifierMouseArgs e)
+        {
+            _selectedSeries = null;
+            base.DeselectAll();
+        }
+    }
+}
