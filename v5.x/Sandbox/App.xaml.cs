679f226fa7b34194bd33dd0a12ff0796222b80b3
andyb1979 <aburnettthompson@gmail.com>
2017-11-22 15:13:14 +0000

    Merging sandbox from v4 to v5

---
+++ v5.x/Sandbox/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using SciChart.Wpf.UI.Bootstrap;
+using SciChart.Wpf.UI.Reactive.Async;
+
+namespace SciChart.Sandbox
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static TestSuiteBootstrapper _bootStrapper;
+
+        public App()
+        {
+            Startup += App_Startup;
+        }
+
+        static void App_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                Container = new UnityContainer();
+                _bootStrapper = new TestSuiteBootstrapper(Container);
+                _bootStrapper.Initialize();
+            }
+            catch (Exception caught)
+            {
+                MessageBox.Show("Exception occurred in Test Suite Startup");
+            }
+        }
+
+        public static UnityContainer Container { get; set; }
+    }
+
+    public class TestSuiteBootstrapper : AbtBootstrapper
+    {
+        public TestSuiteBootstrapper(UnityContainer container) : base(container, null)
+        {
+        }
+
+        public override void Initialize()
+        {
+            // base.Initialize();
+            Container.RegisterInstance<ISchedulerContext>(new SchedulerContext(
+                new SharedScheduler(TaskScheduler.FromCurrentSynchronizationContext(), DispatcherScheduler.Current),
+                new SharedScheduler(TaskScheduler.Default, Scheduler.Default)));
+        }
+    }
+
+}
---
+++ v5.x/Sandbox/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using SciChart.Core.Utility;
+
+namespace SciChart.Sandbox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            this.DataContext = new MainViewModel();
+        }
+
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var chk = (ComboBox)sender;
+            switch (chk.SelectedIndex)
+            {
+                case 0:
+                    SciChartDebugLogger.Instance.SetLogger(null);
+                    break;
+                case 1:
+                    SciChartDebugLogger.Instance.SetLogger(new ConsoleLogger());
+                    break;
+                case 2:
+                    SciChartDebugLogger.Instance.SetLogger(new FileLogger());
+                    break;
+            }
+        }
+
+        private class ConsoleLogger : ISciChartLoggerFacade
+        {
+            public void Log(string formatString, params object[] args)
+            {
+                Console.WriteLine(formatString, args);
+            }
+        }
+
+        private class FileLogger : ISciChartLoggerFacade
+        {
+            private const string LogFileName = "SciChart.log";
+            private const int LogFileAppendMaxSize = 5<<20; // 5 Mb
+
+            private bool _failure;
+
+            public FileLogger()
+            {
+                try
+                {
+                    FileInfo fi = new FileInfo(LogFileName);
+                    if (fi.Exists && fi.Length > LogFileAppendMaxSize)
+                    {
+                        File.Delete(LogFileName);
+                    }
+                }
+                catch (Exception e)
+                {
+                    ReportFailure(e);
+                }
+            }
+
+            private void ReportFailure(Exception e)
+            {
+                _failure = true;
+                Debug.Assert(false, "An error has been occurred in FileLogger: " + e.Message);
+            }
+
+            public void Log(string formatString, params object[] args)
+            {
+                if (!_failure)
+                {
+                    try
+                    {
+                        File.AppendAllText(LogFileName, string.Format(formatString, args) + "\n");
+                    }
+                    catch (Exception e)
+                    {
+                        ReportFailure(e);
+                    }
+                }
+            }
+        }
+
+        private void Control_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var item = this.testCases.SelectedItem;
+            var dataContext = this.DataContext as MainViewModel;
+            if (item != null && dataContext != null)
+            {
+                dataContext.RunExampleCommand.Execute(null);
+            }
+        }
+
+        private void GCCollect_Click(object sender, RoutedEventArgs e)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
