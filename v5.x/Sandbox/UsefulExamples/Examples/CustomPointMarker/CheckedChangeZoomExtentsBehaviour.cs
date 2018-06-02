2189101d611e8c2c4e931261daacc20096f3b03d
Andrew Burnett-Thompson <andrew@scichart.com>
2018-06-02 20:25:18 +0100

    Added custom legend and pointmarker sample

---
+++ v5.x/Sandbox/UsefulExamples/Examples/CustomPointMarker/CheckedChangeZoomExtentsBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using SciChart.Charting.Visuals;
+
+namespace CustomPointMarker
+{
+    /// <summary>
+    /// A behaviour that allows zoom extents on a SciChartSurface when a Visibility Checbox is checked in the Legend
+    /// </summary>
+    public static class CheckedChangeZoomExtentsBehaviour
+    {
+        public static readonly DependencyProperty EnableZoomExtentsOnCheckedProperty = DependencyProperty.RegisterAttached(
+            "EnableZoomExtentsOnChecked", typeof (bool), typeof (CheckedChangeZoomExtentsBehaviour), new PropertyMetadata(default(bool), OnEnableZoomExtentsOnChecked));        
+
+        public static void SetEnableZoomExtentsOnChecked(DependencyObject element, bool value)
+        {
+            element.SetValue(EnableZoomExtentsOnCheckedProperty, value);
+        }
+
+        public static bool GetEnableZoomExtentsOnChecked(DependencyObject element)
+        {
+            return (bool) element.GetValue(EnableZoomExtentsOnCheckedProperty);
+        }
+
+        private static void OnEnableZoomExtentsOnChecked(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var checkbox = d as CheckBox;
+            bool enabled = (bool)e.NewValue;
+
+            checkbox.Checked -= CheckboxChecked;
+            checkbox.Unchecked -= CheckboxChecked;
+            if (enabled)
+            {
+                checkbox.Checked += CheckboxChecked;
+                checkbox.Unchecked += CheckboxChecked;    
+            }
+        }
+
+        private static void CheckboxChecked(object sender, RoutedEventArgs e)
+        {
+            var checkbox = sender as CheckBox;
+            var scs = checkbox.FindVisualParent<SciChartSurface>();
+            if (scs != null) scs.AnimateZoomExtents(TimeSpan.FromMilliseconds(500));
+        }
+
+        private static T FindVisualParent<T>(this DependencyObject dependencyObject)
+            where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(dependencyObject);
+ 
+            if (parent == null) return null;
+ 
+            var parentT = parent as T;
+            return parentT ?? FindVisualParent<T>(parent);
+        }
+    }
+}
---
+++ v5.x/Sandbox/UsefulExamples/Examples/CustomPointMarker/CustomPointMarker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using SciChart.Charting.Visuals.PointMarkers;
+using SciChart.Drawing.Common;
+
+namespace CustomPointMarker
+{
+    /// <summary>
+    /// Renders a diamond shape PointMarker, demonstrating custom BasePointMarker API
+    /// </summary>
+    public class DiamondPointMarker : BasePointMarker
+    {
+        public override void Draw(IRenderContext2D context, IEnumerable<Point> centers)
+        {
+            var fill = context.CreateBrush(Fill);
+            var stroke = context.CreatePen(Stroke, AntiAliasing, (float)StrokeThickness);
+
+            float width2 = (float)(Width * 0.5);
+            float height2 = (float)(Height * 0.5);
+
+            foreach (var center in centers)
+            {
+                double top = center.Y - height2;
+                double bottom = center.Y + height2;
+                double left = center.X - width2;
+                double right = center.X + width2;
+
+                var diamondPoints = new[]
+                {
+                    // Points drawn like this:
+                    // 
+                    //      x0      (x4 in same location as x0)
+                    // 
+                    // x3        x1
+                    //   
+                    //      x2
+  
+                    new Point(center.X, top), // x0
+                    new Point(right, center.Y), // x1
+                    new Point(center.X, bottom), // x2
+                    new Point(left, center.Y), // x3
+                    new Point(center.X, top), // x4 == x0
+                };
+
+                context.FillPolygon(fill, diamondPoints);
+                context.DrawLines(stroke, diamondPoints);
+            }
+        }
+    }
+}
