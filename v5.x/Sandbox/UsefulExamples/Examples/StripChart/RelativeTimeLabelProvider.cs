fa358b825ea7c0abf2b213ba6214a1a0d0414126
Andrew Burnett-Thompson <andrew@scichart.com>
2018-06-02 10:47:50 +0100

    Added Scrolling Stripchart example

---
+++ v5.x/Sandbox/UsefulExamples/Examples/StripChart/RelativeTimeLabelProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SciChart.Charting.Visuals.Axes.LabelProviders;
+
+namespace SciChart.Sandbox.Examples.StripChart
+{
+    public class RelativeTimeLabelProvider : NumericLabelProvider
+    {
+        // Update me with current latest time every time you append data!
+        public double CurrentTime { get; set; }
+
+        public override string FormatCursorLabel(IComparable dataValue)
+        {
+            // dataValue is the actual axis label value, e.g. comes from DataSeries.XValues
+            double value = (double)dataValue;
+            Double relative = (CurrentTime - value);
+
+            return String.Format("t-{0:0.0}", relative);
+        }
+
+        public override string FormatLabel(IComparable dataValue)
+        {
+            // dataValue is the actual axis label value, e.g. comes from DataSeries.XValues
+            double value = (double)dataValue;
+            Double relative = (CurrentTime - value);
+
+            return String.Format("t-{0:0.0}", relative);
+        }
+    }
+}
---
+++ v5.x/Sandbox/UsefulExamples/Readme.md
@@ -0,0 +1,11 @@
+#SciChart: Useful Examples
+
+The solution SciChart.Sandbox.sln contains a number of useful examples and user provided solutions. We update this list from time to time. 
+
+###Build instructions
+
+1. Ensure NuGet feed is set in Visual Studio options 
+
+2. Build and run the SciChart.Sandbox.sln
+
+3. Double click on the example you want to run. It will start in a new window. 
