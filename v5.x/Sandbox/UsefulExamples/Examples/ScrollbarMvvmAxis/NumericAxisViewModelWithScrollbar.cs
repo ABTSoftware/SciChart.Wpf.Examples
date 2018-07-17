f2e545cbaec7ea360c75f43935419cced61f96cc
andyb1979 <aburnettthompson@gmail.com>
2018-07-17 14:39:18 +0100

    Added MVVM Scrollbar example to UsefulExamples

---
+++ v5.x/Sandbox/UsefulExamples/Examples/ScrollbarMvvmAxis/NumericAxisViewModelWithScrollbar.cs
@@ -0,0 +1,19 @@
+﻿using SciChart.Charting.Model.ChartSeries;
+
+namespace SciChart.Sandbox.Examples.ScrollbarMvvmAxis
+{
+    public class NumericAxisViewModelWithScrollbar : NumericAxisViewModel
+    {
+        private bool _hasScrollbar;
+
+        public bool HasScrollbar
+        {
+            get { return _hasScrollbar; }
+            set
+            {
+                _hasScrollbar = value;
+                OnPropertyChanged("HasScrollbar");
+            }
+        }
+    }
+}
