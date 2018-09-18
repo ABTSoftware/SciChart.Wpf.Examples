317ee8b141acff3e7b822bda8d6f3421c593b178
Andrew Burnett-Thompson <andrew@scichart.com>
2018-09-18 13:05:50 +0100

    Updated TouchInputSandbox to have more options

--- v5.x/Sandbox/UsefulExamples/Examples/TouchScreenModifiers/TouchInputSandbox.xaml.cs
+++ v5.x/Sandbox/UsefulExamples/Examples/TouchScreenModifiers/TouchInputSandbox.xaml.cs
@@ -13,20 +13,6 @@ namespace SciChart.Sandbox.Examples.TouchScreenModifiers
         public TouchInputSandbox()
         {
             InitializeComponent();
-
-            // Set a DataSeries of type x=DateTime, y0=Double, y1=double on the RenderableSeries declared in XAML
-            var series = new XyyDataSeries<double, double>();
-            scs2D.RenderableSeries[0].DataSeries = series;
-
-            // Get some data for the upper and lower band
-            var data = DataManager.Instance.GetDampedSinewave(1.0, 0.01, 1000);
-            var moreData = DataManager.Instance.GetDampedSinewave(1.0, 0.005, 1000, 12);
-
-            // Append data to series. SciChart automatically redraws
-            series.Append(
-                data.XData,
-                data.YData,
-                moreData.YData);
         }
     }
 }
