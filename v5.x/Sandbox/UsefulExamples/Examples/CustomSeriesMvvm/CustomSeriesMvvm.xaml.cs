3f173d2e1bfd9ec8bee10927bcdc90fd6acebc8e
Andrew Burnett-Thompson <andrew@scichart.com>
2018-10-29 11:40:58 +0000

    Adding example for deriving from one of our series with MVVM

---
+++ v5.x/Sandbox/UsefulExamples/Examples/CustomSeriesMvvm/CustomSeriesMvvm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SciChart.Sandbox.Examples.CustomSeriesMvvm
+{
+    /// <summary>
+    /// Interaction logic for CustomSeriesMvvm.xaml
+    /// </summary>
+    [TestCase("Custom Series MVVM Example")]
+    public partial class CustomSeriesMvvm : Window
+    {
+        public CustomSeriesMvvm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ v5.x/Sandbox/UsefulExamples/Examples/CustomSeriesMvvm/FastLineRenderableSeriesEx.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using SciChart.Charting.Visuals.RenderableSeries;
+using SciChart.Drawing.Common;
+
+namespace SciChart.Sandbox.Examples.CustomSeriesMvvm
+{
+    /// <summary>
+    /// The series type to create. See <see cref="LineRenderableSeriesViewModelEx"/> ViewType
+    /// </summary>
+    public class FastLineRenderableSeriesEx : FastLineRenderableSeriesForMvvm
+    {
+        protected override void InternalDraw(IRenderContext2D renderContext, IRenderPassData renderPassData)
+        {
+            base.InternalDraw(renderContext, renderPassData);
+            Debug.WriteLine("Drawing! Custom series...");
+        }
+    }
+}
