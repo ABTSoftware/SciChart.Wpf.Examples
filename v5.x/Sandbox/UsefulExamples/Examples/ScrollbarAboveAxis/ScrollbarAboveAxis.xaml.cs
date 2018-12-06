d9af929b3cfa0f8a879ea5ce98db412ad4b498a1
Andrew Burnett-Thompson <andrew@scichart.com>
2018-12-06 11:37:38 +0000

    Improved the scrollbar example

--- v5.x/Sandbox/UsefulExamples/Examples/ScrollbarAboveAxis/ScrollbarAboveAxis.xaml.cs
+++ v5.x/Sandbox/UsefulExamples/Examples/ScrollbarAboveAxis/ScrollbarAboveAxis.xaml.cs
@@ -11,15 +11,35 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SciChart.Charting.Model.DataSeries;
+using SciChart.Charting.Visuals.RenderableSeries;
 
 namespace SciChart.Sandbox.Examples.ScrollbarAboveAxis
 {
+    /// <summary>
+    /// NOTE: In order for this sample to work you need to set a valid developer license. This will hide the POWERED BY SCICHART logo which makes
+    /// the lower chart have a minimum size. 
+    /// </summary>
     [TestCase("Scrollbars above axis")]
     public partial class ScrollbarAboveAxis : Window
     {
         public ScrollbarAboveAxis()
         {
             InitializeComponent();
+
+            primaryChartSurface.RenderableSeries.Add(new FastLineRenderableSeries()
+            {
+                DataSeries=GetData(),
+            });
+        }
+
+        private IDataSeries GetData()
+        {
+            var data = new XyDataSeries<double>();
+            data.Append(0,0);
+            data.Append(1, 1);
+            data.Append(2, 2);
+        return data;
         }
     }
 }
