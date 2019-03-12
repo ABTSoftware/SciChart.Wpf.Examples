c58d7e542fb7e74b864622bece925dd6b0001972
Andrew Burnett-Thompson <andrew@scichart.com>
2019-03-12 18:05:22 +0000

    Added sample to CustomModifierSandbox, zooming mousewheel on axis

---
+++ v5.x/Sandbox/UsefulExamples/Converters/InverseBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SciChart.Sandbox.Converters
+{
+    public class InverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool) value) == false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
