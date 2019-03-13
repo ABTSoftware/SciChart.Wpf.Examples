a5c31eff02300e8e6d500df3e857ae8bf0be329a
Andrew Burnett-Thompson <andrew@scichart.com>
2019-03-13 16:46:10 +0000

    Comments and cleanup

--- v5.x/Sandbox/UsefulExamples/Examples/TimelineControl/TimelineRenderableSeries.cs
+++ v5.x/Sandbox/UsefulExamples/Examples/TimelineControl/TimelineRenderableSeries.cs
@@ -15,13 +15,16 @@ using Point = System.Windows.Point;
 
 namespace SciChart.Sandbox.Examples.TimelineControl
 {
-    public class TimelinePointMetadata : IPointMetadata
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        public bool IsSelected { get; set; }
-        public Color Fill { get; set; }
-    }
-
+    /// <summary>
+    /// A series which displays a timeline of different colours. This series will display from left to right, showing blocks of colour according to the following
+    /// values in an XyzDataSeries
+    ///
+    /// X-value: the start point of the block
+    /// Y-value: used as the length of the block in the X-Axis
+    /// Z-value: used as the color of the block
+    ///
+    /// NOTE The height of the block is defined by properties YOffset and Height which must be set in XAML or code before this chart will draw
+    /// </summary>
     public class TimelineRenderableSeries : CustomRenderableSeries  
     {
         public static readonly DependencyProperty HeightProperty = DependencyProperty.Register(
@@ -42,35 +45,48 @@ namespace SciChart.Sandbox.Examples.TimelineControl
             set { SetValue(YOffsetProperty, value); }
         }
 
-        public TimelineRenderableSeries()
-        {
-        }
-
         protected override void Draw(IRenderContext2D renderContext, IRenderPassData renderPassData)
         {
             base.Draw(renderContext, renderPassData);
 
             // Do the drawing for our timeline here
+            // 
+
+            // Input data is type XyzPointSeries so must be cast
             var inputData = renderPassData.PointSeries as XyzPointSeries;
+
+            // Get X,Y axis calculators 
             var xCalc = renderPassData.XCoordinateCalculator;
             var yCalc = renderPassData.YCoordinateCalculator;
+
+            // Compute some constants
             double yMid = YOffset;
             double halfHeight = Height * 0.5;
-            var metadataCollection = DataSeries.Metadata;
-
+            
+            // Iterate over the data 
             for (int i = 0; i < inputData.Count; i++)
             {
+                // Now compute the bounds of the box to draw for one data-points
+                // XStart = X, XEnd = XStart + Y (we use Y for the length of the box) 
+                // YTop, YBottom defined by Height + YOffset properties
                 double xStartCoord = xCalc.GetCoordinate(inputData.XValues[i]);
                 double xEndCoord = xStartCoord + xCalc.GetCoordinate(inputData.YValues[i]);
                 double yTop = yCalc.GetCoordinate(yMid + halfHeight);
                 double yBottom = yCalc.GetCoordinate(yMid - halfHeight);
 
+                // Get the color for this block
                 int iColor = (int)inputData.ZPoints[i];
                 Color fill = iColor.ToColor();
 
+                // Brush creation can be expensive, cache brushes keyed on int color if you find this
                 using (var scichartBrush = renderContext.CreateBrush(fill))
                 {
+                    // Draw a rectangle 
                     renderContext.FillRectangle(scichartBrush, new Point(xStartCoord, yBottom), new Point(xEndCoord, yTop));
+
+                    // NOTE: 
+                    // You can draw fill, stroke, and fills can be linear gradient brushes if you want. 
+                    // Extra data can be passed through the Metadata parameter in XyzDataSeries
                 }                
             }
         }
@@ -82,6 +98,7 @@ namespace SciChart.Sandbox.Examples.TimelineControl
                 return base.GetXRange();
             }
 
+            // Override the XRange calculation for this series based on the 'width' of the last point remembering that Width = Y
             var xOut = new DoubleRange((double)DataSeries.XValues[0], 
                 (double)DataSeries.XValues[DataSeries.Count - 1] + (double)DataSeries.YValues[DataSeries.Count - 1]);
             return xOut;
