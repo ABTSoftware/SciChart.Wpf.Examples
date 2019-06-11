3c94ba8f7b0d809b7cae3c63d739a87a8a5efa19
YuriyZadereckiy <yurazadereckii@gmail.com>
2019-06-11 18:21:23 +0300

    Modified the "DoubleScaleAxis example" to better show the feature

--- v5.x/Sandbox/UsefulExamples/Examples/DoubleScaleDiscontinuousDateTimeAxis/MainViewModel.cs
+++ v5.x/Sandbox/UsefulExamples/Examples/DoubleScaleDiscontinuousDateTimeAxis/MainViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using SciChart.Charting.Model.DataSeries;
 using SciChart.Charting.Numerics.Calendars;
+using SciChart.Examples.ExternalDependencies.Data;
 
 namespace DoubleScaleDiscontinuousAxis
 {
@@ -7,9 +11,24 @@ namespace DoubleScaleDiscontinuousAxis
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private IDataManager _dataManager = DataManager.Instance;
         private IDiscontinuousDateTimeCalendar _calendar = new WeekDaysAxisCalendar();
+        private IOhlcDataSeries<DateTime, double> _dataSeries;
+
+        public MainViewModel()
+        {
+            var instrument = _dataManager.AvailableInstruments.FirstOrDefault(x => x.Symbol == "INDU");
+            var timeFrame = _dataManager.GetAvailableTimeFrames(instrument).First();
+
+            var priceData = _dataManager.GetPriceData(instrument.Symbol, timeFrame);
+
+            _dataSeries = new OhlcDataSeries<DateTime, double>();
+            _dataSeries.Append(priceData.TimeData, priceData.OpenData, priceData.HighData, priceData.LowData, priceData.CloseData);
+        }
 
         public IDiscontinuousDateTimeCalendar AxisCalendar => _calendar;
+
+        public IDataSeries DataSeries => _dataSeries;
 
         protected virtual void OnPropertyChanged(string propertyName = null)
         {
