d300ff8c2d5b1ee9f9fe6eace97c1a487ce1fc1d
SciChart DevTeam <devteam@scichart.com>
2019-10-18 21:19:13 +0100

    SciChart version 6.0.0.12569

--- Examples/SciChart.Examples/Urls.cs
+++ Examples/SciChart.Examples/Urls.cs
@@ -1,5 +1,8 @@
 ﻿namespace SciChart.Examples
 {
+    /// <summary>
+    /// Place to put web links which are launched at various places throughout the app
+    /// </summary>
     public static class Urls
     {
         public static string GithubRootUrl => "https://github.com/ABTSoftware/SciChart.Wpf.Examples/tree/SciChart_v6_Release";
@@ -7,7 +10,5 @@
         public static string GithubExampleRootUrl => "https://github.com/ABTSoftware/SciChart.Wpf.Examples/tree/SciChart_v6_Release/Examples/SciChart.Examples/Examples/";
 
         public static string DocumentationRootUrl => "https://www.scichart.com/wpf-chart-documentation";
-
-        public static string GoogleSearchUrl => "https://www.google.com/search?q=";
     }
 }
