e0444f8160b6391f1bf2345742109698da84f0bb
SciChart DevTeam <devteam@scichart.com>
2019-10-21 18:05:30 +0100

    SciChart version 6.0.0.12578

---
+++ Examples/SciChart.Examples.ExternalDependencies/Common/LinearToLogarithmicValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SciChart.Examples.ExternalDependencies.Common
+{
+    public class LinearToLogarithmicValueConverter: IValueConverter
+    {
+        public double LogarithmicBase { get; set; } = 2;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IConvertible convertible)
+            {
+                return Math.Pow(LogarithmicBase, convertible.ToDouble(CultureInfo.InvariantCulture));
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IConvertible convertible)
+            {
+                return Math.Log(convertible.ToDouble(CultureInfo.InvariantCulture), LogarithmicBase);
+            }
+
+            return value;
+        }
+    }
+}
