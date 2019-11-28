383d6ef7d2ec550877dbc36c52dba16c28b63c03
SciChart DevTeam <devteam@scichart.com>
2019-11-28 13:16:31 +0000

    Created custom sample for event on zoom extents

---
+++ Sandbox/CustomerExamples/EventOnZoomExtentsCompleted/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using SciChart.Charting.Model.DataSeries;
+
+namespace EventOnZoomExtentsCompleted
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var data = new XyDataSeries<double>();
+            data.Append(0,0);
+            data.Append(1, 1);
+            data.Append(2, 2);
+
+            lineSeries.DataSeries = data;
+        }
+
+        private void OnZoomExtentsCompleted(object sender, EventArgs e)
+        {
+            MessageBox.Show("Zoom extents completed!");
+        }
+    }
+}
