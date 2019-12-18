7092e4db5422f2e2635f843bc71bfdb01632799e
npel <nazariy@scichart.com>
2019-12-18 12:36:47 +0200

    SC-5177 Provide example of SciChartOverview + MVVM + Scrolling strip chart

---
+++ Sandbox/UsefulExamples/Examples/StripChart And SciChartOverViewMVVM/StripChartAndSciChartOverViewMVVM.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SciChart.Wpf.TestSuite.ExampleSandbox.StripChart_And_SciChartOverViewMVVM
+{
+    /// <summary>
+    /// Interaction logic for StripChartAndSciChartOverViewMVVM.xaml
+    /// </summary>
+    [TestCase("StripChart And SciChartOverViewMVVM")]
+    public partial class StripChartAndSciChartOverViewMVVM : Window
+    {
+        public StripChartAndSciChartOverViewMVVM()
+        {
+            InitializeComponent();
+            this.DataContext = new StripChartViewModel();
+        }
+
+        private void OnOverviewSurfaceLoaded(object sender, RoutedEventArgs e)
+        {
+            OverviewSurface.ZoomExtents();
+        }
+    }
+}
