64a2e69a309bf897406910db1a7d7c64278ff8e3
Andrew Burnett-Thompson <andrew@scichart.com>
2020-01-08 22:25:58 +0000

    Updated licensing test app to use v6

--- Sandbox/LicensingTestApp/WinForms/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ Sandbox/LicensingTestApp/WinForms/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -35,18 +35,20 @@
             // 
             this.elementHost.Dock = System.Windows.Forms.DockStyle.Fill;
             this.elementHost.Location = new System.Drawing.Point(0, 0);
+            this.elementHost.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.elementHost.Name = "elementHost";
-            this.elementHost.Size = new System.Drawing.Size(284, 261);
+            this.elementHost.Size = new System.Drawing.Size(1986, 1216);
             this.elementHost.TabIndex = 0;
             this.elementHost.Text = "elementHost1";
             this.elementHost.Child = null;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1986, 1216);
             this.Controls.Add(this.elementHost);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Sandbox/LicensingTestApp/Wpf/WpfApplication1/App.xaml.cs
+++ Sandbox/LicensingTestApp/Wpf/WpfApplication1/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using SciChart.Charting.Visuals;
 
 namespace WpfApplication1
 {
@@ -13,7 +14,9 @@ namespace WpfApplication1
     /// </summary>
     public partial class App : Application
     {
-        public App() { SciChart.Charting.Visuals.SciChartSurface.SetRuntimeLicenseKey(
-            @"<-- Put your LicenseContract here -->"); }
+        public App() 
+        {
+            SciChartSurface.SetRuntimeLicenseKey("pCQC4ntDTBTAslHr0JZOzbtvJ/6rUs4JDchCSeAJ+dQT6PsBtI+Mv5iCDT3yu6Ji4ebsbvZHWJ/dqZd5UvdBsqoU06x496fwLNO5Hyk0bblCVn3vzdw2pTSXXE60YxxtxX9Ft1jf01b4QTkniJTbohMLcZwR81lGozqzU823LVPe4fp8tO6lfiAVzc+usFyjlsbG2D/2eZxREfLrqV1pmS3CA6WMRiGzuOkLML0g1goAptnmHMZeyd+RsI3lovn7CvYKbgQX0eexT0jI6R8JbwCu1RfRSE8/auWyWIN3/Pd+K0MIPfyfsLGwo7Lehd1bOLDv9ctlI5QtLShvEdwuO6IMHchnO0TNLwr/z9lOihUdjsh1BOzlU7JCvldpVY7zF2J8yDb+VFsZC4G9yarEZkBrMf7Lj4GF+m6Rm2Bn3gQWaz+qva1s/j8DuFj2hArvrVJsOTYTmw8clVJrxXrE1MzaGFxxSdjbPwhWPRc=");
+        }
     }
 }
