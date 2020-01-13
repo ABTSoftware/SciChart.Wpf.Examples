15b6151686130012091edabf46422faaa46f21b3
Andrew Burnett-Thompson <andrew@scichart.com>
2020-01-13 13:38:32 +0000

    Updated licensing test apps to use 6.0.1

--- Sandbox/LicensingTestApp/Wpf/WpfApplication1/App.xaml.cs
+++ Sandbox/LicensingTestApp/Wpf/WpfApplication1/App.xaml.cs
@@ -15,8 +15,8 @@ namespace WpfApplication1
     public partial class App : Application
     {
         public App() 
-        {
-            SciChartSurface.SetRuntimeLicenseKey("pCQC4ntDTBTAslHr0JZOzbtvJ/6rUs4JDchCSeAJ+dQT6PsBtI+Mv5iCDT3yu6Ji4ebsbvZHWJ/dqZd5UvdBsqoU06x496fwLNO5Hyk0bblCVn3vzdw2pTSXXE60YxxtxX9Ft1jf01b4QTkniJTbohMLcZwR81lGozqzU823LVPe4fp8tO6lfiAVzc+usFyjlsbG2D/2eZxREfLrqV1pmS3CA6WMRiGzuOkLML0g1goAptnmHMZeyd+RsI3lovn7CvYKbgQX0eexT0jI6R8JbwCu1RfRSE8/auWyWIN3/Pd+K0MIPfyfsLGwo7Lehd1bOLDv9ctlI5QtLShvEdwuO6IMHchnO0TNLwr/z9lOihUdjsh1BOzlU7JCvldpVY7zF2J8yDb+VFsZC4G9yarEZkBrMf7Lj4GF+m6Rm2Bn3gQWaz+qva1s/j8DuFj2hArvrVJsOTYTmw8clVJrxXrE1MzaGFxxSdjbPwhWPRc=");
+        { 
+            //SciChartSurface.SetRuntimeLicenseKey(@"Put your LicenseContract here");
         }
     }
 }
