5168875abd11e4dd15281ddc971d25347b545919
Taras Sobchyshak <taras.sobchyshak@blackthorn-vision.com>
2020-03-10 17:52:44 +0200

    custom series animation example added

---
+++ v5.x/Sandbox/UsefulExamples/Examples/CustomSeriesAnimation/CustomSeriesAnimation.cs
@@ -0,0 +1,48 @@
+﻿using SciChart.Charting.Visuals.RenderableSeries;
+using SciChart.Charting.Visuals.RenderableSeries.Animations;
+using SciChart.Data.Model;
+using System;
+
+namespace SciChart.Sandbox.Examples.CustomSeriesAnimation
+{
+    public class CustomSeriesAnimation : PointSeriesAnimationBase
+    {
+        /// <inheritdoc/>
+        protected override IRenderPassData Animate(IRenderPassData rpd, double currentProgress)
+        {
+            if (rpd?.PointSeries != null)
+            {
+                CustomTransform(rpd.PointSeries, currentProgress);
+            }
+
+            return rpd;
+        }
+
+        private void CustomTransform(IPointSeries pointSeries, double currentProgress)
+        {
+            if (pointSeries.Count > 1)
+            {
+                lock (RenderableSeries.DataSeries.SyncRoot)
+                {
+                    if (pointSeries.Count > 1)
+                    {
+                        var nextPointIndex = pointSeries.Count - 1;
+                        var lastPointIndex = pointSeries.Count - 2;
+
+                        var nextIndex = pointSeries.Indexes[nextPointIndex];
+                        var lastIndex = pointSeries.Indexes[lastPointIndex];
+
+                        var nextXValue = Convert.ToDouble(RenderableSeries.DataSeries.XValues[nextIndex]);
+                        var lastXValue = Convert.ToDouble(RenderableSeries.DataSeries.XValues[lastIndex]);
+
+                        var nextYValue = Convert.ToDouble(RenderableSeries.DataSeries.YValues[nextIndex]);
+                        var lastYValue = Convert.ToDouble(RenderableSeries.DataSeries.YValues[lastIndex]);
+
+                        pointSeries.XValues[nextPointIndex] = lastXValue + currentProgress * (nextXValue - lastXValue);
+                        pointSeries.YValues[nextPointIndex] = lastYValue + currentProgress * (nextYValue - lastYValue);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ v5.x/Sandbox/UsefulExamples/Examples/CustomSeriesAnimation/SeriesAnimation.xaml.cs
@@ -0,0 +1,60 @@
+﻿using SciChart.Charting.Model.DataSeries;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SciChart.Sandbox.Examples.CustomSeriesAnimation
+{
+    [TestCase("Custom SeriesAnimation")]
+    public partial class SeriesAnimation : Window
+    {
+        private int count = 120;
+        private TaskCompletionSource<bool> tcs = null;
+
+        public SeriesAnimation()
+        {
+            InitializeComponent();
+
+            var data = new XyDataSeries<double, double>();
+            for (int i = 0; i < count; i++)
+            {
+                if (i < 31)
+                    data.Append(i, Math.Sin(i * 0.1));
+                else if (i < 95)
+                    data.Append(i, 1.5 * (1.0 + Math.Cos(i * 0.1)));
+                else
+                    data.Append(i, Math.Min(1.0, Math.Sqrt(i) - Math.Sqrt(95)));
+
+            }
+
+            lineSeries.DataSeries = data;
+            lineSeries.DataSeries.DataSeriesChanged += DataSeries_DataSeriesChanged;
+        }
+
+        private void DataSeries_DataSeriesChanged(object sender, DataSeriesChangedEventArgs e)
+        {
+            customAnimation?.StartAnimation();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lineSeries.DataSeries is XyDataSeries<double, double> xySeries)
+            {
+                var n = 100;
+                for (int i = 0; i < n; ++i, ++count)
+                {
+                    tcs = new TaskCompletionSource<bool>();
+                    xySeries.Append(count, 2.0 + Math.Sin(count * 0.2));
+
+                    var result = await tcs.Task;
+                    if (result == false) throw new Exception("Previous animation haven't been completed");
+                }
+            }
+        }
+
+        private void CustomSeriesAnimation_AnimationEnded(object sender, EventArgs e)
+        {
+            tcs?.TrySetResult(true);
+        }
+    }
+}
