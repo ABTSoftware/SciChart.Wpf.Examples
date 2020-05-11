d481116f7d3014435ba062c77908f7551f91ab29
Andrew Burnett-Thompson <andrew@scichart.com>
2020-05-11 16:37:19 +0100

    Added customerexample - Mouse Events on Annotations

---
+++ Sandbox/CustomerExamples/MouseEventsOnAnnotations/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp31
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Control_OnPreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Double Click! " + sender.GetType().Name);
+            e.Handled = true;
+        }
+    }
+}
