9566e8f8e3b22e0b55283ae649c6d15e5fa483b8
Dmytro Herasymenko <dmytro.herasymenko@Blackthorn-vision.com>
2020-06-05 17:57:49 +0300

    SC-5455 Create an example for multi UI threads under Sandbox/CustomerExamples

---
+++ Sandbox/CustomerExamples/MutipleUIThreadExample/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MutipleUIThreadExample
+{
+    public partial class App : Application
+    {
+        public Thread SecondUIThread { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if (SecondUIThread != null) return;
+
+            SecondUIThread = new Thread(() =>
+            {
+                var secondWindow = new SecondWindow();
+
+                secondWindow.Left = (SystemParameters.PrimaryScreenWidth / 2) - (secondWindow.Width / 2) + 100;
+                secondWindow.Top = (SystemParameters.PrimaryScreenHeight / 2) - (secondWindow.Height / 2) + 100;
+                
+                secondWindow.Closed += (s, t) => Dispatcher.CurrentDispatcher.InvokeShutdown();
+
+                secondWindow.Show();
+                secondWindow.Activate();
+
+                Dispatcher.Run();
+            });
+
+            SecondUIThread.SetApartmentState(ApartmentState.STA);
+            SecondUIThread.IsBackground = true;
+            SecondUIThread.Start();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            SecondUIThread?.Abort();
+        }
+
+        public static string CurrentThreadInfo => $"Thread #{Thread.CurrentThread.ManagedThreadId} [{Thread.CurrentThread.GetApartmentState()}]";
+    }
+}
---
+++ Sandbox/CustomerExamples/MutipleUIThreadExample/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace MutipleUIThreadExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Title = $"Main Window | {App.CurrentThreadInfo}";
+
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var dataManager = new DataManager();
+
+            using (sciChartSurface.SuspendUpdates())
+            {
+                sciChartSurface.RenderableSeries[0].DataSeries = dataManager.DataSeriesPork;
+                sciChartSurface.RenderableSeries[1].DataSeries = dataManager.DataSeriesVeal;
+                sciChartSurface.RenderableSeries[2].DataSeries = dataManager.DataSeriesTomato;
+                sciChartSurface.RenderableSeries[3].DataSeries = dataManager.DataSeriesCucumber;
+                sciChartSurface.RenderableSeries[4].DataSeries = dataManager.DataSeriesPepper;
+            }
+
+            sciChartSurface.ZoomExtents();
+        }
+    }
+}
