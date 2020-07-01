96d9e52e1b8a63f9610f0fa2af1b6d1c70b7daab
Andrew Burnett-Thompson <andrew@scichart.com>
2020-07-01 16:36:16 +0100

    Work in progress. Create example for binding to VerticalSliceModifier.VerticalLines

---
+++ Sandbox/CustomerExamples/VerticalSliceModifierMvvm/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SciChart.Charting.Common.Helpers;
+using SciChart.Charting.Model.ChartSeries;
+using SciChart.Charting.Visuals.Annotations;
+using SciChart.Data.Model;
+
+namespace CustomSeriesMvvmExample
+{
+    public class MainViewModel : BindableObject
+    {
+        private Random random = new Random();
+        private readonly ObservableCollection<IAnnotationViewModel> _verticalLines = new ObservableCollection<IAnnotationViewModel>();
+
+        public MainViewModel()
+        {
+            _verticalLines.Add(new VerticalLineAnnotationViewModel() { X1 = 2, ShowLabel = true});
+            _verticalLines.Add(new VerticalLineAnnotationViewModel() { X1 = 5, ShowLabel = true});
+        }
+
+        public ObservableCollection<IAnnotationViewModel> VerticalLines => _verticalLines;
+
+        public ICommand AddLinesCommand
+        {
+            get
+            {
+                return new ActionCommand(() => _verticalLines.Add(new VerticalLineAnnotationViewModel() { X1 = random.NextDouble() * 10, ShowLabel = true }));
+            }
+        }
+
+        public ICommand ClearLinesCommand
+        {
+            get
+            {
+                return new ActionCommand(() => _verticalLines.Clear());
+            }
+        }
+    }
+}
---
+++ Sandbox/CustomerExamples/VerticalSliceModifierMvvm/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CustomSeriesMvvmExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //var lines = new SciChart.Charting.Visuals.Annotations.VerticalLineAnnotationCollection();
+            //lines.Add(new SciChart.Charting.Visuals.Annotations.VerticalLineAnnotation() { X1 = 2 });
+
+            //vsm.VerticalLines = lines;
+        }
+    }
+}
