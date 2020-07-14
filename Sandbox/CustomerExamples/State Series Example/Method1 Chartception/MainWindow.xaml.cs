15924debe1416cf2697005621112c2b0f38e5837
Andrew Burnett-Thompson <andrew@scichart.com>
2020-07-14 17:58:31 +0100

    Added State Series example. Two methods so far, the ChartCeption method and Stacked axis method

---
+++ Sandbox/CustomerExamples/State Series Example/Method1 Chartception/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace State_Series_Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
