44db3f733c33896d2809dc494a1fcb338991cfcd
SciChart DevTeam <devteam@scichart.com>
2020-09-02 12:49:18 +0100

    SciChart version 6.2.2.13328

--- Examples/AssemblyInfoCommon.cs
+++ Examples/AssemblyInfoCommon.cs
@@ -42,4 +42,4 @@ using System.Runtime.InteropServices;
 
 // NOTE: MASTER SHOULD KEEP THIS AS V7.x WHEN MERGING. 
 // This is to prevent conflicts in teamcity when tests are run on both master & v6 branch at the same time. 
-[assembly: AssemblyVersion("6.2.2.13326")]
+[assembly: AssemblyVersion("6.2.2.13328")]
