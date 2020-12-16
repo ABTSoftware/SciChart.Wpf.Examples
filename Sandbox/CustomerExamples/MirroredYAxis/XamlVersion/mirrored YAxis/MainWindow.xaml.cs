42e5da836b0cee43cbcbdfa89d5f5ec2d02feb59
Andrew Burnett-Thompson <andrew@scichart.com>
2020-12-16 19:05:36 +0000

    Added Mirrored YAxis example (showing YAxis on left and right of the chart) both MVVM and code behind versions

---
+++ Sandbox/CustomerExamples/MirroredYAxis/XamlVersion/mirrored YAxis/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SciChart.Charting.Model.DataSeries;
+
+namespace WpfApplication14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var data = new XyDataSeries<double, double>();
+            data.Append(new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, new double[] { 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0 });
+
+            lineSeries.DataSeries = data;
+        }
+    }
+}
