3ed69e9d71b16c297a4c3beacfec83ddc1d66a57
SciChart DevTeam <devteam@scichart.com>
2021-01-06 15:51:49 +0000

    SciChart version 6.3.0.13458

---
+++ Examples/SciChart.Examples/Examples/HeatmapChartTypes/UniformHeatmapPeakDetection/UniformHeatmapPeakDetection.xaml.cs
@@ -0,0 +1,54 @@
+﻿using SciChart.Charting.Model.DataSeries.Heatmap2DArrayDataSeries;
+using SciChart.Examples.ExternalDependencies.Data;
+using System;
+using System.Windows.Controls;
+
+namespace SciChart.Examples.Examples.HeatmapChartTypes.UniformHeatmapPeakDetection
+{
+    public partial class UniformHeatmapPeakDetection : UserControl
+    {
+        public UniformHeatmapPeakDetection()
+        {
+            InitializeComponent();
+
+            var buffer = GetSpectrogramBuffer();
+            HeatmapRenderableSeries.DataSeries = new UniformHeatmapDataSeries<int, int, double>(buffer, 0, 1, 0, 1);
+        }
+
+        private double[,] GetSpectrogramBuffer()
+        {
+            const int yCount = 100;
+            const int xCount = 4_096;
+            var spectrogramBuffer = new double[yCount, xCount];
+
+            var random = new Random();
+            var transform = new FFT2();
+            transform.init(12);
+
+            var re = new double[xCount];
+            var im = new double[xCount];
+            for (int y = 0; y < yCount; y++)
+            {
+                for (int i = 0; i < xCount; i++)
+                {
+                    re[i] = 2d * Math.Sin(2 * Math.PI * i / 20) +
+                            5 * Math.Sin(2 * Math.PI * i / 10) -
+                            2d * random.NextDouble();
+                    im[i] = -10;
+                }
+
+                transform.run(re, im);
+
+                for (int i = 0; i < xCount; i++)
+                {
+                    var mag = Math.Sqrt(re[i] * re[i] + im[i] * im[i]);
+                    im[i] = i;
+
+                    spectrogramBuffer[y, i] = 20 * Math.Log10(mag / xCount);
+                }
+            }
+
+            return spectrogramBuffer;
+        }
+    }
+}
