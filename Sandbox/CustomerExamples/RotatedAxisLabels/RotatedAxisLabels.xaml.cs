3956e606ba7376297e86b0f408d23937044377a4
Andrew Burnett-Thompson <andrew@scichart.com>
2022-03-01 12:45:47 +0000

    Added rotated labels example

---
+++ Sandbox/CustomerExamples/RotatedAxisLabels/RotatedAxisLabels.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using SciChart.Charting.Model.DataSeries;
+using SciChart.Charting.Visuals.Axes.LabelProviders;
+
+namespace RotatedAxisLabelsExample
+{
+    public partial class RotatedAxisLabels : Window
+    {
+        public StringsOnXAxis()
+        {
+            InitializeComponent();
+        }
+    }
+}
