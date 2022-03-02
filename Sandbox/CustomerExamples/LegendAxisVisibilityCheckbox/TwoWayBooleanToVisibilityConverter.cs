7442ed9667371f23f2a9424f78ccf7ba4a5d5663
Andrew Burnett-Thompson <andrew@scichart.com>
2022-03-02 18:22:04 +0000

    Added LegendAxisVisibilityCheckbox example

---
+++ Sandbox/CustomerExamples/LegendAxisVisibilityCheckbox/TwoWayBooleanToVisibilityConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LegendAxisVisibilityCheckboxExample
+{
+    public class TwoWayBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility)
+            {
+                return (Visibility)value == Visibility.Visible ? true : false;
+            }
+
+            if (value is bool)
+            {
+                return (bool)value == true ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility)
+            {
+                return (Visibility)value == Visibility.Visible ? true : false;
+            }
+
+            if (value is bool)
+            {
+                return (bool)value == true ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
