b0d65dee051a691755712376cead29a8fc381333
Nathan0dev <nathan.dev@scichart.com>
2022-12-21 15:28:00 +0200

    SC-6977 After connecting new dlls to the sunbox examples, the color of the theme changes plus minor fixes, refactoring, renaming

--- Sandbox/CustomerExamples/StrokeDashArrayPointMarkers/StrokeDashArrayPointMarkers/MainWindow.xaml.cs
+++ Sandbox/CustomerExamples/StrokeDashArrayPointMarkers/StrokeDashArrayPointMarkers/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace StrokeDashArrayPointMarkers
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
