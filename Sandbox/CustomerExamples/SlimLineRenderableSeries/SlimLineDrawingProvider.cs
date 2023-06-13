7ca5c768e969a85d0149d51b6c166c35ea6527a4
Demien B <demi.dev@scichart.com>
2023-06-13 13:30:37 +0300

    SC-7349 Create a sandbox example that demonstrates usage of the SlimLineRenderableSeries

---
+++ Sandbox/CustomerExamples/SlimLineRenderableSeries/SlimLineDrawingProvider.cs
@@ -0,0 +1,12 @@
+﻿using SciChart.Charting.Visuals.RenderableSeries.DrawingProviders;
+using SciChart.Data.Model;
+
+namespace SlimLineRenderableSeriesExample
+{
+    public class SlimLineDrawingProvider : LineSeriesDrawingProvider<Point2DSeries>
+    {
+        public SlimLineDrawingProvider(SlimLineRenderableSeries renderSeries) : base(renderSeries)
+        {
+        }
+    }
+}
